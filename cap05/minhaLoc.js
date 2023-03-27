c134893c7f7760e65ebf9b37a3407398c11f02c0
Assis Moreira <assisu@gmail.com>
2023-03-27 17:01:03 -0300

    ts

--- cap05/minhaLoc.js
+++ cap05/minhaLoc.js
@@ -2,22 +2,41 @@ window.onload = buscaMinhaLocalizacao;
 
 function buscaMinhaLocalizacao(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(exibeLocalizacao, exibeErro);
+        var botaoVisao = document.getElementById("visao");
+        botaoVisao.onclick = visaoLocalizacao;
+        var botaoLimparVisao = document.getElementById("limparVisao");
+        botaoLimparVisao.onclick = limparVisao;
     }else{
-        alert("Ops, localização não suportada")
+        alert("Ops, localização não suportada");
     }
-
 }
+var visaoId =null;
+function visaoLocalizacao(){
+    visaoId = navigator.geolocation.watchPosition(exibeLocalizacao, exibeErro, {timeout:500000});
+}
+function limparVisao(){
+    if(visaoId){
+        navigator.geolocation.clearWatch(visaoId);
+        visaoId =null;
+    }
+}
+
 function exibeLocalizacao(position){
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
     var div = document.getElementById("localizacao");
     div.innerHTML = "Latidude:" + latitude + " e " + "longitude:"+ longitude;
+    div.innerHTML += "(com " + position.coords.accuracy + " metros de precisação)"; 
 
     var km = computeDistance(position.coords, ourCoords);
     var distancia = document.getElementById("distancia");
     distancia.innerHTML = "Você está" + km + " km de distancia de HQ";
+
+    if(mapa == null){   
+        verMapa(position.coords);
+    }
+    
 }
 function exibeErro(error){
     var tiposErros = {
@@ -55,3 +74,40 @@ var ourCoords = {
     longitude:
     -122.52099
 };
+
+
+var mapa;
+
+function verMapa(coords) {
+    var googleLateLong =  new google.maps.LatLng(coords.latitude,coords.longitude);
+    var mapaOpcoes = {
+        zoom: 10,
+        center: googleLateLong,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var mapaDiv = document.getElementById("mapa");
+    mapa = new google.maps.Map(mapaDiv, mapaOpcoes);
+    var titulo = "Minha Localização";
+    var conteudo = "Você está aqui:" + coords.latitude + "," + coords.longitude;
+
+    adiconaMarcador(mapa, googleLateLong, titulo, conteudo);
+}
+
+function adiconaMarcador (mapa, latlong, titulo, conteudo){
+    var marcadorOpcoes = {
+        position: latlong,
+        map: mapa,
+        title: titulo,
+        clickable: true
+    };
+    var marcador = new google.maps.Marker(marcadorOpcoes);
+    var informacoesJanelaOpcoes = {
+        content: conteudo,
+        position: latlong
+    };
+    var infoJanela = new google.maps.InfoWindow(informacoesJanelaOpcoes);
+    google.maps.event.addListener(marcador, "click", function(){
+            infoJanela.open(mapa);
+        }
+    )
+}
