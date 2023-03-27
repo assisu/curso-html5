76fc41a4e72b47fb6547e27bf70a48c41ff4a2b9
Assis Moreira <assisu@gmail.com>
2023-03-27 17:42:08 -0300

    cap06

---
+++ cap06/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var url ="https://curso-html5.vercel.app/cap06/sales.json";
+    var requisicao = new XMLHttpRequest();
+    requisicao.open("GET", url);
+    requisicao.onload = function(){
+        if(requisicao.status ==200){
+            alert(requisicao.responseText);
+        }
+    };
+    requisicao.send(null)
+}
