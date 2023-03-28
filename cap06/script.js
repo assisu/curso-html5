3da24f6623fb12965c7a9216f5a5a368831a07a7
Assis Moreira <assisu@gmail.com>
2023-03-28 09:35:36 -0300

    f

--- cap06/script.js
+++ cap06/script.js
@@ -1,17 +1,8 @@
 window.onload = function(){
-    var url ="https://curso-html5.vercel.app/cap06/sales.json";
-    var requisicao = new XMLHttpRequest();
-    requisicao.open("GET", url);
-    requisicao.onload = function(){
-        if(requisicao.status ==200){
-            atulizaVendas(requisicao.responseText);
-        }
-    };
-    requisicao.send(null)
+    
 }
-function atulizaVendas (responseText){
+function atulizaVendas (vendas){
     var vendasDiv = document.getElementById("vendas");
-    var vendas = JSON.parse(responseText);
     for(var i=0; i< vendas.length; i++){
         var venda = vendas[i];
         var div = document.createElement("div");
