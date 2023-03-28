36ea946f444b8f6c969e6cec79a7616107e00f63
Assis Moreira <assisu@gmail.com>
2023-03-27 22:08:02 -0300

    10

--- cap06/script.js
+++ cap06/script.js
@@ -4,12 +4,19 @@ window.onload = function(){
     requisicao.open("GET", url);
     requisicao.onload = function(){
         if(requisicao.status ==200){
-            alert(requisicao.responseText);
+            atulizaVendas(requisicao.responseText);
         }
     };
     requisicao.send(null)
 }
 function atulizaVendas (responseText){
     var vendasDiv = document.getElementById("vendas");
-    vendasDiv.innerHTML = responseText;
+    var vendas = JSON.parse(responseText);
+    for(var i=0; i< vendas.length; i++){
+        var venda = vendas[i];
+        var div = document.createElement("div");
+        div.setAttribute("class", "vendaItem");
+        div.innerHTML = venda.name + " vendida " + venda.sales + " chicletes";
+        vendasDiv.appendChild(div);
+    }
 }
